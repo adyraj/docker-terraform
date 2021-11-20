046f5f6c488d767f8951b5df96b1943d6ff758ed
Aditya Raj <adyraj02@gmail.com>
2021-11-20 18:56:33 +0530

    Test 17

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 resource "null_resource" "example1" {
   provisioner "local-exec" {
-    command = "docker version"
+    command = "docker"
     interpreter = ["/bin/bash"]
   }
 }
